364c318f531d166761db2979426318261775f254
Rohan Suryawanshi <126396108+rohansuryawanshi08@users.noreply.github.com>
2024-11-22 14:20:04 +0530

    Create 05constantvariable.sh

---
+++ 05constantvariable.sh
@@ -0,0 +1,7 @@
+# constant variable is variable which we dont't want to change until end of script WE USE (readonly )befor writing variable
+
+readonly COLLEGE= "MIT"
+echo "my collage is $ COLLAGE
+
+
+#
