c94cd7ab2d1de647455baa9af34a37b33a138872
Rohan Suryawanshi <126396108+rohansuryawanshi08@users.noreply.github.com>
2024-11-29 20:42:16 +0530

    Create 17userintraction.sh

---
+++ 17userintraction.sh
@@ -0,0 +1,12 @@
+# we use read or echo to take input
+
+echo "what is your name " 
+read name
+echo "your name is $name"
+
+
+      #or 
+
+read -p "what is your name " name 
+echo "your name is $name "
+
