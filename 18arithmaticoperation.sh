6c3d2f1b52b7dd2ee9741e7c7929d95b8a4b2bee
Rohan Suryawanshi <126396108+rohansuryawanshi08@users.noreply.github.com>
2024-11-29 20:57:57 +0530

    Create 18arithmaticoperation.sh

---
+++ 18arithmaticoperation.sh
@@ -0,0 +1,12 @@
+# for doing arithmatic operation we use "let" or we can directily do by using (()) 
+
+x=10
+y=9
+
+let sum=$x+$y
+echo "$sum"
+
+        #or
+ echo "muliplication is $(( $x*$y ))
+
+echo " substraction is $(( $x-$y ))"
