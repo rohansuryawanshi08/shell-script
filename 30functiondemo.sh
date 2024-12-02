7fb22e51a2cc5fa5c91350806958860bc6d9c456
Rohan Suryawanshi <126396108+rohansuryawanshi08@users.noreply.github.com>
2024-12-02 20:52:46 +0530

    Create 30functiondemo.sh

---
+++ 30functiondemo.sh
@@ -0,0 +1,12 @@
+# welcome note
+
+function welcomenote {
+        echo " _________________"
+        echo "____welcome_______"
+        echo "___________________"
+      
+}
+
+
+welcomenote
+welcomenote
