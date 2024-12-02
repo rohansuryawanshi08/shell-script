f2e1026ec81c4c5e009d8fcc9ddc0da79fe7bfcf
Rohan Suryawanshi <126396108+rohansuryawanshi08@users.noreply.github.com>
2024-12-02 20:57:45 +0530

    Create 31functionargument.sh

---
+++ 31functionargument.sh
@@ -0,0 +1,12 @@
+
+
+function welcomenote {
+        echo " _________________"
+        echo "____welcome  $1_______"
+        echo "___________________"
+      
+}
+
+
+welcomenote raju
+welcomenote shyam
